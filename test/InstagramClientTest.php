f7c21d851eda3df72d22f14e882c5ca35f1964fe
Jabran Rafique <jabran@influence.digital>
2014-12-01 12:47:30 +0000

    add test

---
+++ test/InstagramClientTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Unit test suite
+ * @author: hello@jabran.me
+ */
+
+require('../autoload.php');
+
+class InstagramClientCTest extends PHPUnit_Framework_TestCase {
+	public $config, $client;
+
+	public function setUp( $config = array() ) {
+		$this->config = $config;
+
+		$this->client = new InstagramClient( $this->config );
+	}
+
+	public function tearDown() {
+		unset($this->client);
+	}
+}
